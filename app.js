b4a09c4ac7fbc1762030cfbd9e593214697cb8b3
Will Franceschini <>
2023-03-21 10:35:40 -0700

    added palindrome test

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 console.log("linked!")
 
 // export default sum;
-export default reverseStr;
+// export default reverseStr;
+export default isPalindrome;
 
 // function sum(a, b) {
 //     return a + b - 1;
 // }
 
-function reverseStr(str) {
-    let stringArr = str.split("");
+// function reverseStr(str) {
+//     let stringArr = str.split("");
+//     let reversedArr = stringArr.reverse();
+//     let joinArr = reversedArr.join("");
+//     return joinArr;
+// }
+
+function isPalindrome(sequence) {
+    if (typeof sequence === "number") {
+        sequence = sequence.toString();
+    }
+    let stringArr = sequence.split("");
     let reversedArr = stringArr.reverse();
     let joinArr = reversedArr.join("");
-    return joinArr;
+    return joinArr === sequence;
 }
--- test.js
+++ test.js
@@ -1,9 +1,17 @@
 import { describe, expect, test} from "vitest";
 // import sum from "app.js";
-import reverseStr from "app.js";
+// import reverseStr from "app.js";
+import isPalindrome from "./app";
 
 describe("Test case: reversing a string", function() {
-    test("reverseStr", function() {
-        expect(reverseStr("Hello")).toEqual("olleH");
-    })
-})
+    test("should return true", function() {
+        expect(isPalindrome("121")).toEqual(true);
+        expect(isPalindrome("1221")).toEqual(true);
+        expect(isPalindrome("madam")).toEqual(true);
+    });
+    test("should return false", function() {
+        expect(isPalindrome("Hello")).toEqual(false);
+        expect(isPalindrome("sup!")).toEqual(false);
+        expect(isPalindrome("3467")).toEqual(false);
+    });
+});
